24b71918e5d008fc7334aa7f0fe795c1b8e68d05
Robert Säll <pr_125@hotmail.com>
2017-06-16 12:59:37 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+To re-create:
+
+- npm run
+- focus the element and write something, keep it focused
+- wait for the setInterval to fire
+- now the input will be deselected and the span is rendered
---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import { RIEInput } from 'riek'
+
+class App extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      name: 'test'
+    }
+
+    setInterval(() => {
+      console.log('updating state...')
+      this.setState({
+        otherProp: 'otherValue'
+      })
+    }, 2000)
+
+    this.handleNameChange = newName => {
+      this.setState({
+        name: newName
+      })
+    }
+  }
+
+  render () {
+    return (
+      <RIEInput
+        value={this.state.name}
+        change={this.handleNameChange}
+        propName='text'
+        shouldRemainWhileInvalid={true}
+         />
+    )
+  }
+}
+
+export default App
